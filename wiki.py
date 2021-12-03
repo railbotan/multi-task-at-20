d6cdf4ad2d43da0574b316fdda2879e5c1adaf33
romcq <nl.pastuxow@gmail.com>
2021-12-03 21:42:19 +0500

    task

--- wiki.py
+++ wiki.py
@@ -1,17 +1,22 @@
-from urllib.request import Request, urlopen
+import urllib.request
 from urllib.parse import unquote
+import concurrent.futures
 
 links = open('res.txt', encoding='utf8').read().split('\n')
 
-for url in links:
-    try:
-        request = Request(
-            url,
-            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
-        )
-        resp = urlopen(request, timeout=5)
-        code = resp.code
-        print(code)
-        resp.close()
-    except Exception as e:
-        print(url, e)
+
+def load_url(link, timeout):
+    with urllib.request.urlopen(link, timeout=timeout) as conn:
+        return conn.code
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    future_to_url = {executor.submit(load_url, url, 5): url for url in links}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            data = future.result()
+        except Exception as e:
+            print('%r exception: %s' % (url, e))
+        else:
+            print(data)
